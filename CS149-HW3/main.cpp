67354704871a35e1083d2bb8999bd85749a71a3e
Luca Severini <lucaseverini@mac.com>
2014-02-28 17:59:24 -0800

    Initial Commit

---
+++ CS149-HW3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS149-HW3
+//
+//  Created by Luca Severini on 2/28/14.
+//  Copyright (c) 2014 Luca Severini. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
